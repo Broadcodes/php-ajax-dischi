f73f42c3891a6e69dc90f49eafb73827d323a31f
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-17 17:31:33 +0200

    Creazione dell'api

---
+++ secondVersion/index.php
@@ -0,0 +1,38 @@
+<!-- Seconda Versione: Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà,
+     attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+     prima milestone: recuperare la lista dei dischi tramite una chiamata axios e stampare la lista dei titoli
+     degli album all'interno di una lista non numerata (ul)
+     seconda milestone: per ogni disco, stampare una card con tutte le informazioni -->
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotybool</title>
+
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <script type="text/javascript" src="../firstVersion/script.js" defer></script>
+</head>
+
+<body>
+    <header>
+        <i class="logoImg fa-brands fa-spotify"></i>
+    </header>
+
+    <main>
+        <div class="container">
+            <?php
+          
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
