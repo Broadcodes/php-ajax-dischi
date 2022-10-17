40a3c2ad9329701a103568510ffa79d52db57fde
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-17 15:50:42 +0200

    Inizializzazione prima versione

---
+++ firstVersion/index.php
@@ -0,0 +1,30 @@
+<!-- Prima Versione: Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina:
+     al caricamento della pagina ci saranno tutti i dischi. -->
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Spotybool</title>
+
+   <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+   <link rel="stylesheet" href="../css/style.css">
+
+   <script type="text/javascript" src="../firstVersion/script.js" defer></script>
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
